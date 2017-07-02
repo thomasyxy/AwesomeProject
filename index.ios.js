2695faddb4237ca95328ddac14e8cc8c7d6feac6
yinxueyuan <838476901@qq.com>
2017-07-02 23:44:23 +0800

    picker

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,141 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  PickerIOS,
   Text,
-  View
-} from 'react-native';
+  View,
+} = ReactNative;
 
-export default class AwesomeProject extends Component {
-  render() {
+var PickerItemIOS = PickerIOS.Item;
+
+var CAR_MAKES_AND_MODELS = {
+  amc: {
+    name: 'AMC',
+    models: ['AMX', 'Concord', 'Eagle', 'Gremlin', 'Matador', 'Pacer'],
+  },
+  alfa: {
+    name: 'Alfa-Romeo',
+    models: ['159', '4C', 'Alfasud', 'Brera', 'GTV6', 'Giulia', 'MiTo', 'Spider'],
+  },
+  aston: {
+    name: 'Aston Martin',
+    models: ['DB5', 'DB9', 'DBS', 'Rapide', 'Vanquish', 'Vantage'],
+  },
+  audi: {
+    name: 'Audi',
+    models: ['90', '4000', '5000', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q5', 'Q7'],
+  },
+  austin: {
+    name: 'Austin',
+    models: ['America', 'Maestro', 'Maxi', 'Mini', 'Montego', 'Princess'],
+  },
+  borgward: {
+    name: 'Borgward',
+    models: ['Hansa', 'Isabella', 'P100'],
+  },
+  buick: {
+    name: 'Buick',
+    models: ['Electra', 'LaCrosse', 'LeSabre', 'Park Avenue', 'Regal',
+             'Roadmaster', 'Skylark'],
+  },
+  cadillac: {
+    name: 'Cadillac',
+    models: ['Catera', 'Cimarron', 'Eldorado', 'Fleetwood', 'Sedan de Ville'],
+  },
+  chevrolet: {
+    name: 'Chevrolet',
+    models: ['Astro', 'Aveo', 'Bel Air', 'Captiva', 'Cavalier', 'Chevelle',
+             'Corvair', 'Corvette', 'Cruze', 'Nova', 'SS', 'Vega', 'Volt'],
+  },
+};
+
+var PickerExample = React.createClass({
+  getInitialState: function() {
+    return {
+      carMake: 'cadillac',
+      modelIndex: 3,
+    };
+  },
+
+  render: function() {
+    var make = CAR_MAKES_AND_MODELS[this.state.carMake];
+    var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Text>Please choose a make for your car:</Text>
+        <PickerIOS
+          selectedValue={this.state.carMake}
+          onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
+          {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
+            <PickerItemIOS
+              key={carMake}
+              value={carMake}
+              label={CAR_MAKES_AND_MODELS[carMake].name}
+            />
+          ))}
+        </PickerIOS>
+        <Text>Please choose a model of {make.name}:</Text>
+        <PickerIOS
+          selectedValue={this.state.modelIndex}
+          key={this.state.carMake}
+          onValueChange={(modelIndex) => this.setState({modelIndex})}>
+          {CAR_MAKES_AND_MODELS[this.state.carMake].models.map((modelName, modelIndex) => (
+            <PickerItemIOS
+              key={this.state.carMake + '_' + modelIndex}
+              value={modelIndex}
+              label={modelName}
+            />
+          ))}
+        </PickerIOS>
+        <Text>You selected: {selectionString}</Text>
       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+var PickerStyleExample = React.createClass({
+  getInitialState: function() {
+    return {
+      carMake: 'cadillac',
+      modelIndex: 0,
+    };
+  },
+
+  render: function() {
+    var make = CAR_MAKES_AND_MODELS[this.state.carMake];
+    var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
+    return (
+      <PickerIOS
+        itemStyle={{fontSize: 25, color: 'red', textAlign: 'left', fontWeight: 'bold'}}
+        selectedValue={this.state.carMake}
+        onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
+        {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
+          <PickerItemIOS
+            key={carMake}
+            value={carMake}
+            label={CAR_MAKES_AND_MODELS[carMake].name}
+          />
+        ))}
+      </PickerIOS>
+    );
+  },
+});
+
+exports.displayName = (undefined: ?string);
+exports.title = '<PickerIOS>';
+exports.description = 'Render lists of selectable options with UIPickerView.';
+exports.examples = [
+{
+  title: '<PickerIOS>',
+  render: function(): ReactElement<any> {
+    return <PickerExample />;
+  },
+},
+{
+  title: '<PickerIOS> with custom styling',
+  render: function(): ReactElement<any> {
+    return <PickerStyleExample />;
+  },
+}];
