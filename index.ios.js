76215b0a84576c986db6ca2020ed0d0a3c5a0f4c
yinxueyuan <838476901@qq.com>
2017-07-02 23:49:07 +0800

    button

--- index.ios.js
+++ index.ios.js
@@ -1,141 +1,88 @@
 'use strict';
 
-var React = require('react');
-var ReactNative = require('react-native');
-var {
-  PickerIOS,
-  Text,
+const React = require('react');
+const ReactNative = require('react-native');
+const {
+  Alert,
+  Button,
   View,
 } = ReactNative;
 
-var PickerItemIOS = PickerIOS.Item;
-
-var CAR_MAKES_AND_MODELS = {
-  amc: {
-    name: 'AMC',
-    models: ['AMX', 'Concord', 'Eagle', 'Gremlin', 'Matador', 'Pacer'],
-  },
-  alfa: {
-    name: 'Alfa-Romeo',
-    models: ['159', '4C', 'Alfasud', 'Brera', 'GTV6', 'Giulia', 'MiTo', 'Spider'],
-  },
-  aston: {
-    name: 'Aston Martin',
-    models: ['DB5', 'DB9', 'DBS', 'Rapide', 'Vanquish', 'Vantage'],
-  },
-  audi: {
-    name: 'Audi',
-    models: ['90', '4000', '5000', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q5', 'Q7'],
-  },
-  austin: {
-    name: 'Austin',
-    models: ['America', 'Maestro', 'Maxi', 'Mini', 'Montego', 'Princess'],
-  },
-  borgward: {
-    name: 'Borgward',
-    models: ['Hansa', 'Isabella', 'P100'],
-  },
-  buick: {
-    name: 'Buick',
-    models: ['Electra', 'LaCrosse', 'LeSabre', 'Park Avenue', 'Regal',
-             'Roadmaster', 'Skylark'],
-  },
-  cadillac: {
-    name: 'Cadillac',
-    models: ['Catera', 'Cimarron', 'Eldorado', 'Fleetwood', 'Sedan de Ville'],
-  },
-  chevrolet: {
-    name: 'Chevrolet',
-    models: ['Astro', 'Aveo', 'Bel Air', 'Captiva', 'Cavalier', 'Chevelle',
-             'Corvair', 'Corvette', 'Cruze', 'Nova', 'SS', 'Vega', 'Volt'],
-  },
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
 };
 
-var PickerExample = React.createClass({
-  getInitialState: function() {
-    return {
-      carMake: 'cadillac',
-      modelIndex: 3,
-    };
-  },
+exports.displayName = 'ButtonExample';
+exports.framework = 'React';
+exports.title = '<Button>';
+exports.description = 'Simple React Native button component.';
 
-  render: function() {
-    var make = CAR_MAKES_AND_MODELS[this.state.carMake];
-    var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
-    return (
-      <View>
-        <Text>Please choose a make for your car:</Text>
-        <PickerIOS
-          selectedValue={this.state.carMake}
-          onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
-          {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
-            <PickerItemIOS
-              key={carMake}
-              value={carMake}
-              label={CAR_MAKES_AND_MODELS[carMake].name}
-            />
-          ))}
-        </PickerIOS>
-        <Text>Please choose a model of {make.name}:</Text>
-        <PickerIOS
-          selectedValue={this.state.modelIndex}
-          key={this.state.carMake}
-          onValueChange={(modelIndex) => this.setState({modelIndex})}>
-          {CAR_MAKES_AND_MODELS[this.state.carMake].models.map((modelName, modelIndex) => (
-            <PickerItemIOS
-              key={this.state.carMake + '_' + modelIndex}
-              value={modelIndex}
-              label={modelName}
-            />
-          ))}
-        </PickerIOS>
-        <Text>You selected: {selectionString}</Text>
-      </View>
-    );
-  },
-});
-
-var PickerStyleExample = React.createClass({
-  getInitialState: function() {
-    return {
-      carMake: 'cadillac',
-      modelIndex: 0,
-    };
-  },
-
-  render: function() {
-    var make = CAR_MAKES_AND_MODELS[this.state.carMake];
-    var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
-    return (
-      <PickerIOS
-        itemStyle={{fontSize: 25, color: 'red', textAlign: 'left', fontWeight: 'bold'}}
-        selectedValue={this.state.carMake}
-        onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
-        {Object.keys(CAR_MAKES_AND_MODELS).map((carMake) => (
-          <PickerItemIOS
-            key={carMake}
-            value={carMake}
-            label={CAR_MAKES_AND_MODELS[carMake].name}
-          />
-        ))}
-      </PickerIOS>
-    );
-  },
-});
-
-exports.displayName = (undefined: ?string);
-exports.title = '<PickerIOS>';
-exports.description = 'Render lists of selectable options with UIPickerView.';
 exports.examples = [
-{
-  title: '<PickerIOS>',
-  render: function(): ReactElement<any> {
-    return <PickerExample />;
+  {
+    title: 'Simple Button',
+    description: 'The title and onPress handler are required. It is ' +
+      'recommended to set accessibilityLabel to help make your app usable by ' +
+      'everyone.',
+    render: function() {
+      return (
+        <Button
+          onPress={onButtonPress}
+          title="Press Me"
+          accessibilityLabel="See an informative alert"
+        />
+      );
+    },
   },
-},
-{
-  title: '<PickerIOS> with custom styling',
-  render: function(): ReactElement<any> {
-    return <PickerStyleExample />;
+  {
+    title: 'Adjusted color',
+    description: 'Adjusts the color in a way that looks standard on each ' +
+      'platform. On iOS, the color prop controls the color of the text. On ' +
+      'Android, the color adjusts the background color of the button.',
+    render: function() {
+      return (
+        <Button
+          onPress={onButtonPress}
+          title="Press Purple"
+          color="#841584"
+          accessibilityLabel="Learn more about purple"
+        />
+      );
+    },
   },
-}];
+  {
+    title: 'Fit to text layout',
+    description: 'This layout strategy lets the title define the width of ' +
+      'the button',
+    render: function() {
+      return (
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Button
+            onPress={onButtonPress}
+            title="This looks great!"
+            accessibilityLabel="This sounds great!"
+          />
+          <Button
+            onPress={onButtonPress}
+            title="Ok!"
+            color="#841584"
+            accessibilityLabel="Ok, Great!"
+          />
+        </View>
+      );
+    },
+  },
+  {
+    title: 'Disabled Button',
+    description: 'All interactions for the component are disabled.',
+    render: function() {
+      return (
+        <Button
+          disabled
+          onPress={onButtonPress}
+          title="I Am Disabled"
+          accessibilityLabel="See an informative alert"
+        />
+      );
+    },
+  },
+];
