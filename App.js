7452cdfd35d2a6735ff63573a915e8769231a4bb
yinxueyuan <838476901@qq.com>
2017-11-29 20:10:30 +0800

    feat: add flow

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+// import { colors } from 'config';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
